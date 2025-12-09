d0e9cd9d8ba011b21481a2a40925384e8909e8aa
kjfsm <kjfsm@users.noreply.github.com>
2025-12-09 07:11:58 +0000

    format

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
-  server:{
-    host : true,
+  server: {
+    host: true,
     port: 5173,
     strictPort: true,
     watch: {
       usePolling: true,
     },
-  }
+  },
 });
